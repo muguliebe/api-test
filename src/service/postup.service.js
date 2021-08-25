79100013c018c5b66f86e5205faf5716eb273fb5
zany <muguliebe@gmail.com>
2021-08-26 02:06:10 +0900

    o

--- src/service/postup.service.js
+++ src/service/postup.service.js
@@ -1,26 +1,58 @@
 import axios from 'axios'
+import * as child from 'child_process'
 
 export default class PostUpService {
 
     #instance = null
 
     constructor() {
+        const ip = '127.0.0.1'
+        const port = '8000'
         this.#instance = axios.create({
-            baseURL: 'http://172.30.1.42:8000',
-            timeout: 5000,
+            baseURL: `http://${ip}:${port}`,
+            timeout: 30000,
             headers: {'Content-Type': 'application/json'}
         })
     }
 
     async postUp() {
         logger.debug('postUp] strart')
-        const res = await this.#instance.request({
-            method: 'get',
-            url: '/btt'
-        })
+        // const res = await this.#instance.request({
+        //     method: 'get',
+        //     url: '/btt'
+        // })
+        //
+        // logger.debug('status:' + res.status+ ' ' + res.statusText)
+        // console.dir(res.data)
 
-        logger.debug('status:' + res.status + ' ' + res.statusMessage)
-        console.dir(res.data)
+        const res = await this.spawnChild()
+
+        logger.debug(res)
+        logger.debug('postUp] end 2ssddsa1' )
 
     }
+
+    async spawnChild() {
+        const {spawn} = require('child_process')
+        const child = spawn('python3', ['yo3.py'])
+
+        let data = ''
+        for await (const chunk of child.stdout) {
+            data += chunk
+        }
+        let error = ''
+        for await (const chunk of child.stderr) {
+            error += chunk
+        }
+        const exitCode = await new Promise((resolve, reject) => {
+            child.on('close', resolve)
+        })
+
+        if (exitCode) {
+            throw new Error(`subprocess error exit ${exitCode}, ${error}`)
+        }
+        return data
+    }
+
+
 }
